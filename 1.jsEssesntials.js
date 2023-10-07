02c5451251e0d81553d306c57f93c6b5ad4db31b
Fernando <ferg.developer@gmail.com>
2023-10-07 13:31:28 -0400

    defining variables

---
+++ 1.jsEssesntials.js
@@ -0,0 +1,22 @@
+//varibles are calues in the code
+
+firstName = "Mike";
+x = 2;
+
+// variables can be replaced
+firstName = "Thomas";
+x = 10.0;
+
+
+
+// declaring variables
+
+let myCar = "BMW";
+myCar = "Mercedes Benz";
+
+
+//varibles defining keywords
+let meaningLet = "has a block scope";
+var meaningVar = "has a global scope"
+const meaningConst = "can be only assigned onces"
+
