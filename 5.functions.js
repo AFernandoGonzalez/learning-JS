4cf929ddbd4030690cfd34f2e77614104b68ecf8
Fernando <ferg.developer@gmail.com>
2023-10-31 22:26:41 -0400

    excercice about unclean and clean functions

--- 5.functions.js
+++ 5.functions.js
@@ -40,3 +40,61 @@ addButton.addEventListener('click', handleAddition);
 subtractButton.addEventListener('click', handleSubtraction);
 multiplyButton.addEventListener('click', handleMultiplication);
 divideButton.addEventListener('click', handleDivision);
+
+
+
+// Practicing Functions:
+
+// Unclean Function:
+function checkAvailability(products, selectedProduct) {
+    let isAvailable = false;
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === selectedProduct.id ) {
+            if (products[i].quantity > 0) {
+                isAvailable = true;
+                break;
+            }
+        }
+    }
+    return isAvailable;
+}
+
+// clean function
+function checkAvailability2(products, selectedProduct) {
+    for (let i = 0; i < products.length; i++){
+        if(products[i].id === selectedProduct.id && products[i].quantity >  0){
+            return true;
+        }
+    }
+    return false
+}
+
+
+// Sample data
+const products = [
+    { id: 1, quantity: 0 },
+    { id: 2, quantity: 3 },
+    { id: 3, quantity: 5 },
+];
+const selectedProduct = { id: 2 };
+
+
+console.log(checkAvailability(products, selectedProduct));
+console.log(checkAvailability2(products, selectedProduct));
+
+
+
+
+
+// Unclean Function:
+function formatFullName(firstName, lastName) {
+    if (firstName && lastName) {
+        return firstName + ' ' + lastName;
+    } else if (firstName && !lastName) {
+        return firstName;
+    } else if (!firstName && lastName) {
+        return lastName;
+    } else {
+        return '';
+    }
+}
