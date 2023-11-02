90bea4fce99bd1447b998e05256d92a1a360de05
Fernando <ferg.developer@gmail.com>
2023-11-02 14:02:56 -0400

    function binding from book eloquentjavascript

--- 5.functions.js
+++ 5.functions.js
@@ -636,3 +636,51 @@ console.log(wrap1());
 console.log(wrap2());
 console.log(wrapValue(1)());
 console.log(wrapValue(2)());
+
+
+
+
+const recursionFunc = (base, exponent)=>{
+    if(exponent == 0 ){
+        return 1
+    }else{
+        return base* recursionFunc(base,exponent -1)
+    }
+}
+
+console.log("recursionFunc: ", recursionFunc(2,3));
+
+
+
+
+function findSolution(target) {
+    function find(current, history) {
+        if (current == target) {
+            return history;
+        } else if (current > target) {
+            return null;
+        } else {
+            return find(current + 5, `(${history} + 5)`) ||
+                find(current * 3, `(${history} * 3)`);
+        }
+    }
+    return find(1, "1");
+}
+
+console.log(findSolution(24))
+
+
+
+function printFarmInventory(cows, chickens) {
+    let cowString = String(cows);
+    while (cowString.length < 3) {
+        cowString = "0" + cowString;
+    }
+    console.log(`${cowString} Cows`);
+    let chickenString = String(chickens);
+    while (chickenString.length < 3) {
+        chickenString = "0" + chickenString;
+    }
+    console.log(`${chickenString} Chickens`);
+}
+printFarmInventory(7, 11);
