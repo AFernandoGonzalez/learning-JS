7da609dff78e3ec13a97ddd81bbecf8866261983
Fernando <ferg.developer@gmail.com>
2023-11-02 15:31:16 -0400

    an array of book objects with dummy data has been created

---
+++ exercises/bookstore/bookstore.js
@@ -0,0 +1,32 @@
+
+// create an object to represent books, 
+// and use functions to 
+// add books to the object, 
+// list books, and 
+// display book information in the HTML.
+
+
+const books = [
+    {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        description: "The story of the mysteriously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan.",
+        ISBN: "9780743273565",
+        year: 1925,
+    },
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        description: "The story of racial injustice and the loss of innocence in the American South during the 1930s.",
+        ISBN: "9780061120084",
+        year: 1960,
+    },
+    {
+        title: "1984",
+        author: "George Orwell",
+        description: "A dystopian novel that explores the consequences of a totalitarian regime, mass surveillance, and thought control.",
+        ISBN: "9780451524935",
+        year: 1949,
+    },
+    // Add more books here...
+];
