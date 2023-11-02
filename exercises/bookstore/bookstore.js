846871494e5a9b2b6126530d3d1c2079e75c2f17
Fernando <ferg.developer@gmail.com>
2023-11-02 17:01:48 -0400

    created a bookstore with vanilla js

--- exercises/bookstore/bookstore.js
+++ exercises/bookstore/bookstore.js
@@ -1,6 +1,6 @@
 
-// create an object to represent books, 
-// and use functions to 
+
+// use functions to 
 // add books to the object, 
 // list books, and 
 // display book information in the HTML.
@@ -28,5 +28,62 @@ const books = [
         ISBN: "9780451524935",
         year: 1949,
     },
-    // Add more books here...
 ];
+
+
+const titleInput = document.getElementById('titleInput')
+const authorInput = document.getElementById('authorInput')
+const descriptionInput = document.getElementById('descriptionInput')
+const isbnInput = document.getElementById('isbnInput')
+const yearInput = document.getElementById('yearInput')
+const addButton = document.getElementById('addButton')
+
+const bookList = document.getElementById('bookList')
+
+const displayBooks = () => {
+
+    books.map((book) => {
+        const bookItem = document.createElement('li')
+        bookList.appendChild(bookItem)
+
+        const divContainer = document.createElement('div')
+        const headingTitle = document.createElement('h2')
+        const headingAuthor = document.createElement('h3')
+        const paragraphDesc = document.createElement('p')
+        const spanISBN = document.createElement('span')
+        const spanYear = document.createElement('span')
+
+        bookItem.appendChild(divContainer)
+        bookItem.appendChild(headingTitle)
+        bookItem.appendChild(headingAuthor)
+        bookItem.appendChild(paragraphDesc)
+        bookItem.appendChild(spanISBN)
+        bookItem.appendChild(spanYear)
+
+        // bookItem.innerText = book.title
+        headingTitle.innerText = book.title
+        headingAuthor.innerText = book.author
+        paragraphDesc.innerText = book.description
+        spanISBN.innerText = book.ISBN
+        spanYear.innerText = book.year
+    })
+
+
+    const addBook = () => {
+        const newBook = {
+            title: titleInput.value,
+            author: authorInput.value,
+            description: descriptionInput.value,
+            ISBN: isbnInput.value,
+            year: yearInput.value
+        }
+        books.push(newBook)
+
+        displayBooks()
+    }
+
+    addButton.addEventListener('click', addBook)
+    
+}
+
+displayBooks()
