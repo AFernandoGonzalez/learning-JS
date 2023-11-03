c83798eabce79fc470988e4fee74953cbdf71393
Fernando <ferg.developer@gmail.com>
2023-11-03 19:44:12 -0400

    practice exercise 3.1

--- 2.multipleValues.js
+++ 2.multipleValues.js
@@ -37,9 +37,27 @@ convertButton.addEventListener('click', handleFahrenheitConversion);
 
 
 
-newNamesArray = new Array(5)
-// newNamesArray = ['Jo', 'Fe', 'Tr', 10]
+// let newNamesArray = new Array(5)
+newNamesArray = ['Jo', 'Fe', 'Tr', 10, 'soup']
 console.log(newNamesArray);
 
 const myInitials = newNamesArray[1]
 console.log(myInitials);
+
+
+//length
+console.log("length of array: ", newNamesArray.length);
+console.log("last index of array: ", newNamesArray[newNamesArray.length - 1])
+
+
+// Practice exercise 3.1
+
+// Create an array to use as your shopping list with 3 items: "Milk," "Bread," and
+// "Apples."
+let shoppingList = ["Milk", "Bread","Apples"]
+// 2. Check your list length in the console.
+console.log("my shopping list length: ", shoppingList.length);
+// 3. Update "Bread" to "Bananas."
+shoppingList[1] = "Bananas"
+// 4. Output your entire list to the console
+console.log("updated shopping list: ", shoppingList);
